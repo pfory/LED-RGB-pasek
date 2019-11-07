d29d6106715c44d7f1517d24b6a27f928a480ff6
Petr <pfory@seznam.cz>
2019-11-07 15:09:52 +0100

    redesign aplikace

---
+++ Configuration.h
@@ -0,0 +1,80 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+//SW name & version
+#define     VERSION                      "0.01"
+#define     SW_NAME                      "RGBLEDSTRIP1"
+
+#define timers
+#define ota
+#define verbose
+
+
+#define AUTOCONNECTNAME   HOSTNAMEOTA
+#define AUTOCONNECTPWD    "password"
+
+#define ota
+#ifdef ota
+#include <ArduinoOTA.h>
+#define HOSTNAMEOTA   "RGBLEDSTRIP1"
+#endif
+
+/*
+--------------------------------------------------------------------------------------------------------------------------
+
+Version history:
+
+--------------------------------------------------------------------------------------------------------------------------
+HW
+ESP8266-01
+RGB LED strip
+*/
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <Ticker.h>
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+#include "DoubleResetDetector.h" // https://github.com/datacute/DoubleResetDetector
+#include "Sender.h"
+#include <Wire.h>
+#include <PubSubClient.h>
+
+#define verbose
+#ifdef verbose
+  #define DEBUG_PRINT(x)         Serial.print (x)
+  #define DEBUG_PRINTDEC(x)      Serial.print (x, DEC)
+  #define DEBUG_PRINTLN(x)       Serial.println (x)
+  #define DEBUG_PRINTF(x, y)     Serial.printf (x, y)
+  #define DEBUG_PRINTHEX(x)      Serial.print (x, HEX)
+  #define PORTSPEED 115200
+  #define SERIAL_BEGIN           Serial.begin(PORTSPEED);
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTDEC(x)
+  #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRINTF(x, y)
+#endif 
+
+static const char* const      mqtt_server                    = "192.168.1.56";
+static const uint16_t         mqtt_port                      = 1883;
+static const char* const      mqtt_username                  = "datel";
+static const char* const      mqtt_key                       = "hanka12";
+static const char* const      mqtt_base                      = "/home/rgb1";
+static const char* const      static_ip                      = "192.168.1.122";
+static const char* const      static_gw                      = "192.168.1.1";
+static const char* const      static_sn                      = "255.255.255.0";
+static const char* const      mqtt_topic_state               = "";
+static const char* const      mqtt_topic_set                 = "set";
+
+
+
+#define CONFIG_MQTT_PAYLOAD_ON  "ON"
+#define CONFIG_MQTT_PAYLOAD_OFF "OFF"
+
+
+// Pins
+#define CONFIG_PIN_RED          0
+#define CONFIG_PIN_GREEN        2
+#define CONFIG_PIN_BLUE         3
+
+#endif
---
+++ LED_RGB_pasek.ino
@@ -0,0 +1,280 @@
+/*RGB pasek
+--------------------------------------------------------------------------------------------------------------------------
+Petr Fory pfory@seznam.cz
+GIT - https://github.com/pfory/LED_RGB_pasek
+//ESP8266-01
+POZOR na verzi desky esp8266 2.42+, nefunguje interrupt, až do vyřešení nepřecházet na vyšší verzi
+*/
+
+/*TODO
+*/
+
+#include "Configuration.h"
+
+#ifdef time
+#include <TimeLib.h>
+#include <Timezone.h>
+WiFiUDP EthernetUdp;
+static const char     ntpServerName[]       = "tik.cesnet.cz";
+//const int timeZone = 2;     // Central European Time
+//Central European Time (Frankfurt, Paris)
+TimeChangeRule        CEST                  = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+TimeChangeRule        CET                   = {"CET", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+Timezone CE(CEST, CET);
+unsigned int          localPort             = 8888;  // local port to listen for UDP packets
+time_t getNtpTime();
+#endif
+
+// Real values to write to the LEDs (ex. including brightness and state)
+byte realRed = 0;
+byte realGreen = 0;
+byte realBlue = 0;
+
+const int redPin = CONFIG_PIN_RED;
+//const int txPin = BUILTIN_LED; // On-board blue LED
+const int greenPin = CONFIG_PIN_GREEN;
+const int bluePin = CONFIG_PIN_BLUE;
+
+bool stateOn = false;
+
+
+bool isDebugEnabled()
+{
+#ifdef verbose
+  return true;
+#endif // verbose
+  return false;
+}
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+#include <timer.h>
+auto timer = timer_create_default(); // create a timer with default settings
+Timer<> default_timer; // save as above
+
+//MQTT callback
+void callback(char* topic, byte* payload, unsigned int length) {
+  char * pEnd;
+  long int valL;
+  String val =  String();
+  DEBUG_PRINT("Message arrived [");
+  DEBUG_PRINT(topic);
+  DEBUG_PRINT("] ");
+  for (int i=0;i<length;i++) {
+    DEBUG_PRINT((char)payload[i]);
+    val += (char)payload[i];
+  }
+  DEBUG_PRINTLN();
+  
+  if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_topic_restart)).c_str())==0) {
+    printMessageToLCD(topic, val);
+    DEBUG_PRINT("RESTART");
+    saveConfig();
+    ESP.restart();
+  }
+  
+    if (!processJson(message)) {
+    return;
+  }
+
+  if (stateOn) {
+    // Update lights
+    realRed = map(red, 0, 255, 0, brightness);
+    realGreen = map(green, 0, 255, 0, brightness);
+    realBlue = map(blue, 0, 255, 0, brightness);
+  }
+  else {
+    realRed = 0;
+    realGreen = 0;
+    realBlue = 0;
+  }
+
+  startFade = true;
+  inFade = false; // Kill the current fade
+
+  sendState();
+}
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+//----------------------------------------------------- S E T U P -----------------------------------------------------------
+void setup() {
+  // put your setup code here, to run once:
+  SERIAL_BEGIN;
+  DEBUG_PRINT(F(SW_NAME));
+  DEBUG_PRINT(F(" "));
+  DEBUG_PRINTLN(F(VERSION));
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  //pinMode(txPin, OUTPUT);
+  //digitalWrite(txPin, HIGH); // Turn off the on-board LED
+
+
+  ticker.attach(1, tick);
+  //bool _dblreset = drd.detectDoubleReset();
+    
+  WiFi.printDiag(Serial);
+    
+  bool validConf = readConfig();
+  if (!validConf) {
+    DEBUG_PRINTLN(F("ERROR config corrupted"));
+  }
+  
+  rst_info *_reset_info = ESP.getResetInfoPtr();
+  uint8_t _reset_reason = _reset_info->reason;
+  DEBUG_PRINT("Boot-Mode: ");
+  DEBUG_PRINTLN(_reset_reason);
+  heartBeat = _reset_reason;
+  
+ /*
+ REASON_DEFAULT_RST             = 0      normal startup by power on 
+ REASON_WDT_RST                 = 1      hardware watch dog reset 
+ REASON_EXCEPTION_RST           = 2      exception reset, GPIO status won't change 
+ REASON_SOFT_WDT_RST            = 3      software watch dog reset, GPIO status won't change 
+ REASON_SOFT_RESTART            = 4      software restart ,system_restart , GPIO status won't change 
+ REASON_DEEP_SLEEP_AWAKE        = 5      wake up from deep-sleep 
+ REASON_EXT_SYS_RST             = 6      external system reset 
+  */
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  
+  //WiFiManager
+  WiFiManager wifiManager;
+  
+  IPAddress _ip,_gw,_sn;
+  _ip.fromString(static_ip);
+  _gw.fromString(static_gw);
+  _sn.fromString(static_sn);
+
+  wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
+  
+  DEBUG_PRINTLN(_ip);
+  DEBUG_PRINTLN(_gw);
+  DEBUG_PRINTLN(_sn);
+
+  wifiManager.setAPCallback(configModeCallback);
+  
+  if (!wifiManager.autoConnect(AUTOCONNECTNAME, AUTOCONNECTPWD)) { 
+    DEBUG_PRINTLN("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  } 
+
+#ifdef time
+  DEBUG_PRINTLN("Setup TIME");
+  EthernetUdp.begin(localPort);
+  DEBUG_PRINT("Local port: ");
+  DEBUG_PRINTLN(EthernetUdp.localPort());
+  DEBUG_PRINTLN("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300);
+  
+  printSystemTime();
+#endif
+
+#ifdef ota
+  ArduinoOTA.setHostname(HOSTNAMEOTA);
+
+  ArduinoOTA.onStart([]() {
+    DEBUG_PRINTLN("Start updating ");
+  });
+  ArduinoOTA.onEnd([]() {
+   DEBUG_PRINTLN("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    DEBUG_PRINTF("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    DEBUG_PRINTF("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) DEBUG_PRINTLN("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) DEBUG_PRINTLN("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) DEBUG_PRINTLN("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) DEBUG_PRINTLN("Receive Failed");
+    else if (error == OTA_END_ERROR) DEBUG_PRINTLN("End Failed");
+  });
+  ArduinoOTA.begin();
+#endif
+
+}
+
+//----------------------------------------------------- L O O P -----------------------------------------------------------
+void loop() {
+#ifdef serverHTTP
+  server.handleClient();
+#endif
+
+#ifdef ota
+  ArduinoOTA.handle();
+#endif
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+} //loop
+
+
+void sendState() {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["state"] = (stateOn) ? on_cmd : off_cmd;
+  JsonObject& color = root.createNestedObject("color");
+  color["r"] = red;
+  color["g"] = green;
+  color["b"] = blue;
+
+  root["brightness"] = brightness;
+
+  if (colorfade) {
+    if (transitionTime == CONFIG_COLORFADE_TIME_SLOW) {
+      root["effect"] = "colorfade_slow";
+    }
+    else {
+      root["effect"] = "colorfade_fast";
+    }
+  }
+  else {
+    root["effect"] = "null";
+  }
+
+  char buffer[root.measureLength() + 1];
+  root.printTo(buffer, sizeof(buffer));
+
+  client.publish(light_state_topic, buffer, true);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    DEBUG_PRINT("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(mqtt_base, mqtt_username, mqtt_key)) {
+      DEBUG_PRINTLN("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic","hello world");
+      // ... and resubscribe
+      //client.subscribe(mqtt_base + '/' + 'inTopic');
+      client.subscribe((String(mqtt_base) + "/" + "").c_str());
+      client.subscribe((String(mqtt_base) + "/" + "set").c_str());
+    } else {
+      DEBUG_PRINT("failed, rc=");
+      DEBUG_PRINT(client.state());
+      DEBUG_PRINTLN(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
