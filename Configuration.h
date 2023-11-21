3d565d24c6d4c0847d98df1997884ab388001e2a
pfory <pfory@seznam.cz>
2023-11-21 19:48:18 +0100

    +

--- Configuration.h
+++ Configuration.h
@@ -12,13 +12,15 @@ GIT - https://github.com/pfory/LED_RGB_pasek
 */
 
 //SW name & version
-#define     VERSION                      "0.10"
-#define     SW_NAME                      "RGBLEDSTRIP1"
+#define     VERSION                      "0.3"
+#define     SW_NAME                      "RGBLEDSTRIP1 "
 
 #define timers
 #define ota
 #define verbose
 #include <DoubleResetDetector.h>      //https://github.com/khoih-prog/ESP_DoubleResetDetector
+// #include "cred.h"
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
 
 #define AUTOCONNECTNAME   HOSTNAMEOTA
 #define AUTOCONNECTPWD    "password"
@@ -34,62 +36,15 @@ GIT - https://github.com/pfory/LED_RGB_pasek
 #include <Timezone.h>
 #endif
 
-/*
---------------------------------------------------------------------------------------------------------------------------
-
-Version history:
-
---------------------------------------------------------------------------------------------------------------------------
-HW
-ESP8266-01
-RGB LED strip
-*/
-
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
-#include <Ticker.h>
-#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
-#include "DoubleResetDetector.h" // https://github.com/datacute/DoubleResetDetector
-#include "Sender.h"
-#include <Wire.h>
-#include <PubSubClient.h>
-
-#define verbose
-#ifdef verbose
-  #define DEBUG_PRINT(x)         Serial.print (x)
-  #define DEBUG_PRINTDEC(x)      Serial.print (x, DEC)
-  #define DEBUG_PRINTLN(x)       Serial.println (x)
-  #define DEBUG_PRINTF(x, y)     Serial.printf (x, y)
-  #define DEBUG_PRINTHEX(x)      Serial.print (x, HEX)
-  #define PORTSPEED 115200
-  #define SERIAL_BEGIN           Serial.begin(PORTSPEED);
-#else
-  #define DEBUG_PRINT(x)
-  #define DEBUG_PRINTDEC(x)
-  #define DEBUG_PRINTLN(x)
-  #define DEBUG_PRINTF(x, y)
-#endif 
-
-// Number of seconds after reset during which a
-// subseqent reset will be considered a double reset.
-#define DRD_TIMEOUT 2
-// RTC Memory Address for the DoubleResetDetector to use
-#define DRD_ADDRESS 0
-
-#define CONFIG_PORTAL_TIMEOUT 60 //jak dlouho zustane v rezimu AP nez se cip resetuje
-#define CONNECT_TIMEOUT 120 //jak dlouho se ceka na spojeni nez se aktivuje config portal
-
 static const char* const      mqtt_server                    = "192.168.1.56";
 static const uint16_t         mqtt_port                      = 1883;
 static const char* const      mqtt_username                  = "datel";
 static const char* const      mqtt_key                       = "hanka12";
 static const char* const      mqtt_base                      = "/home/rgb1";
-// static const char* const      static_ip                      = "192.168.1.122";
-// static const char* const      static_gw                      = "192.168.1.1";
-// static const char* const      static_sn                      = "255.255.255.0";
-static const char* const      mqtt_topic_state               = "";
-static const char* const      mqtt_topic_set                 = "set";
 static const char* const      mqtt_topic_restart             = "restart";
+static const char* const      mqtt_topic_netinfo             = "netinfo";
+static const char* const      mqtt_config_portal             = "config";
+static const char* const      mqtt_config_portal_stop        = "disconfig";
 static const char* const      mqtt_topic_load                = "load";
 
 
@@ -105,5 +60,7 @@ uint32_t              lastConnectAttempt          = 0;
 #define CONFIG_PIN_RED          0
 #define CONFIG_PIN_GREEN        2
 #define CONFIG_PIN_BLUE         3
+
+#include <fce.h>
 
 #endif
--- LED-RGB-pasek.ino
+++ LED-RGB-pasek.ino
@@ -1,25 +1,13 @@
 #include "Configuration.h"
 
-uint32_t heartBeat                          = 0;
-
-DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
-
-#ifdef time
-WiFiUDP EthernetUdp;
-static const char     ntpServerName[]       = "tik.cesnet.cz";
-//const int timeZone = 2;     // Central European Time
-//Central European Time (Frankfurt, Paris)
-TimeChangeRule        CEST                  = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
-TimeChangeRule        CET                   = {"CET", Last, Sun, Oct, 3, 60};       //Central European Standard Time
-Timezone CE(CEST, CET);
-unsigned int          localPort             = 8888;  // local port to listen for UDP packets
-time_t getNtpTime();
-#endif
+//ESP8266-01  1M FS:none OTA 502kB
 
 // Real values to write to the LEDs (ex. including brightness and state)
 byte          realRed           = 0;
 byte          realGreen         = 0;
 byte          realBlue          = 0;
+byte          smer              = 0;
+byte          effect            = 0;
 
 const int     redPin            = CONFIG_PIN_RED;
 const int     greenPin          = CONFIG_PIN_GREEN;
@@ -43,205 +31,49 @@ const int     bluePin           = CONFIG_PIN_BLUE;
 // int           redVal, grnVal, bluVal;
 
 
-bool isDebugEnabled()
-{
-#ifdef verbose
-  return true;
-#endif // verbose
-  return false;
-}
-
-//for LED status
-#include <Ticker.h>
-Ticker ticker;
-
-void tick()
-{
-  //toggle state
-  int state = digitalRead(bluePin);  // get the current state of GPIO1 pin
-  digitalWrite(bluePin, !state);     // set pin to the opposite state
-}
-
-//gets called when WiFiManager enters configuration mode
-void configModeCallback (WiFiManager *myWiFiManager) {
-  DEBUG_PRINTLN("Entered config mode");
-  DEBUG_PRINTLN(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  DEBUG_PRINTLN(myWiFiManager->getConfigPortalSSID());
-  //entered config mode, make led toggle faster
-  ticker.attach(0.2, tick);
-}
-
-
-#include <timer.h>
-auto timer = timer_create_default(); // create a timer with default settings
-Timer<> default_timer; // save as above
-
 //MQTT callback
 void callback(char* topic, byte* payload, unsigned int length) {
-  char * pEnd;
   String val =  String();
   DEBUG_PRINT("\nMessage arrived [");
   DEBUG_PRINT(topic);
   DEBUG_PRINT("] ");
-  for (int i=0;i<length;i++) {
+  for (unsigned int i=0;i<length;i++) {
     DEBUG_PRINT((char)payload[i]);
     val += (char)payload[i];
   }
   DEBUG_PRINTLN();
   
   if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_topic_restart)).c_str())==0) {
-    //printMessageToLCD(topic, val);
     DEBUG_PRINT("RESTART");
-    //saveConfig();
     ESP.restart();
+  } else if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_topic_netinfo)).c_str())==0) {
+    DEBUG_PRINT("NET INFO");
+    sendNetInfoMQTT();
+  } else if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_config_portal)).c_str())==0) {
+    startConfigPortal();
+  } else if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_config_portal_stop)).c_str())==0) {
+    stopConfigPortal();
   } else if (strcmp(topic, (String(mqtt_base) + "/" + String(mqtt_topic_load)).c_str())==0) {
     processJson(val);
-    // if (val.substring(0,1)=="r") {
-      // analogWrite(redPin, val.substring(1).toInt());
-      // analogWrite(greenPin, 0);
-      // analogWrite(bluePin, 0);
-    // } else if (val.substring(0,1)=="g") {
-      // analogWrite(redPin, 0);
-      // analogWrite(greenPin, val.substring(1).toInt());
-      // analogWrite(bluePin, 0);
-    // } else if (val.substring(0,1)=="b") {
-      // analogWrite(redPin, 0);
-      // analogWrite(greenPin, 0);
-      // analogWrite(bluePin, val.substring(1).toInt());
-    // }
-  // } else if (mymode == 'f') {   // f;8;255;34
-    // ExtractValues(2, 3);
-    // SetColor(atoi(vals[0]), atoi(vals[1]), atoi(vals[2]));
   }  
-
-
-
-  // if (stateOn) {
-    // // Update lights
-    // realRed = map(red, 0, 255, 0, brightness);
-    // realGreen = map(green, 0, 255, 0, brightness);
-    // realBlue = map(blue, 0, 255, 0, brightness);
-  // }
-  // else {
-    // realRed = 0;
-    // realGreen = 0;
-    // realBlue = 0;
-  // }
-
-  // startFade = true;
-  // inFade = false; // Kill the current fade
-
-  //sendState();
 }
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-WiFiManager wifiManager;
+ADC_MODE(ADC_VCC); //vcc read
 
 //----------------------------------------------------- S E T U P -----------------------------------------------------------
 void setup() {
-  // put your setup code here, to run once:
-  SERIAL_BEGIN;
-  DEBUG_PRINT(F(SW_NAME));
-  DEBUG_PRINT(F(" "));
-  DEBUG_PRINTLN(F(VERSION));
+  preSetup();
+  client.setCallback(callback);
 
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
-  ticker.attach(1, tick);
-  //pinMode(txPin, OUTPUT);
-  //digitalWrite(txPin, HIGH); // Turn off the on-board LED
+  digitalWrite(redPin, 0);
+  digitalWrite(greenPin, 0);
+  digitalWrite(bluePin, 0);
 
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  wifiManager.setAPCallback(configModeCallback);
-  wifiManager.setConfigPortalTimeout(CONFIG_PORTAL_TIMEOUT);
-  wifiManager.setConnectTimeout(CONNECT_TIMEOUT);
-
-  if (drd.detectDoubleReset()) {
-    DEBUG_PRINTLN("Double reset detected, starting config portal...");
-    ticker.attach(0.2, tick);
-    if (!wifiManager.startConfigPortal(HOSTNAMEOTA)) {
-      DEBUG_PRINTLN("failed to connect and hit timeout");
-      delay(3000);
-      //reset and try again, or maybe put it to deep sleep
-      ESP.reset();
-      delay(5000);
-    }
-  }
-  
-  rst_info *_reset_info = ESP.getResetInfoPtr();
-  uint8_t _reset_reason = _reset_info->reason;
-  DEBUG_PRINT("Boot-Mode: ");
-  DEBUG_PRINTLN(_reset_reason);
-  heartBeat = _reset_reason;
-  
- /*
- REASON_DEFAULT_RST             = 0      normal startup by power on 
- REASON_WDT_RST                 = 1      hardware watch dog reset 
- REASON_EXCEPTION_RST           = 2      exception reset, GPIO status won't change 
- REASON_SOFT_WDT_RST            = 3      software watch dog reset, GPIO status won't change 
- REASON_SOFT_RESTART            = 4      software restart ,system_restart , GPIO status won't change 
- REASON_DEEP_SLEEP_AWAKE        = 5      wake up from deep-sleep 
- REASON_EXT_SYS_RST             = 6      external system reset 
-  */
-
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-
-  WiFi.printDiag(Serial);
-
-  if (!wifiManager.autoConnect(AUTOCONNECTNAME, AUTOCONNECTPWD)) { 
-    DEBUG_PRINTLN("failed to connect and hit timeout");
-    delay(3000);
-    //reset and try again, or maybe put it to deep sleep
-    ESP.reset();
-    delay(5000);
-  } 
-
-  sendNetInfoMQTT();
-
-#ifdef time
-  DEBUG_PRINTLN("Setup TIME");
-  EthernetUdp.begin(localPort);
-  DEBUG_PRINT("Local port: ");
-  DEBUG_PRINTLN(EthernetUdp.localPort());
-  DEBUG_PRINTLN("waiting for sync");
-  setSyncProvider(getNtpTime);
-  setSyncInterval(300);
-  
-  printSystemTime();
-#endif
-
-#ifdef ota
-  ArduinoOTA.setHostname(HOSTNAMEOTA);
-
-  ArduinoOTA.onStart([]() {
-    DEBUG_PRINTLN("Start updating ");
-  });
-  ArduinoOTA.onEnd([]() {
-   DEBUG_PRINTLN("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    DEBUG_PRINTF("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    DEBUG_PRINTF("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) DEBUG_PRINTLN("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) DEBUG_PRINTLN("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) DEBUG_PRINTLN("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) DEBUG_PRINTLN("Receive Failed");
-    else if (error == OTA_END_ERROR) DEBUG_PRINTLN("End Failed");
-  });
-  ArduinoOTA.begin();
-  ticker.detach();
-  
-  //test
+//test
   for (uint8_t i=0; i<255; i++) {
     analogWrite(redPin, i);
     delay(5);
@@ -259,19 +91,25 @@ void setup() {
     delay(5);
   }
   analogWrite(bluePin, 0);
-  #endif
 
+
+#ifdef timers
 //setup timers
   timer.every(SENDSTAT_DELAY, sendStatisticMQTT);
-
-  void * a;
+#endif
+  
+  void * a=0;
+  reconnect(a);
   sendStatisticMQTT(a);
+  sendNetInfoMQTT();
   
-  DEBUG_PRINTLN(" Ready");
- 
+  ticker.detach();
+  //keep LED on
+  digitalWrite(LED_BUILTIN, HIGH);
+
   drd.stop();
-  
-  DEBUG_PRINTLN(F("Setup end."));
+
+  DEBUG_PRINTLN(F("SETUP END......................."));
 }
 
 //----------------------------------------------------- L O O P -----------------------------------------------------------
@@ -285,8 +123,32 @@ void loop() {
   ArduinoOTA.handle();
 #endif
 
-  reconnect();
   client.loop();
+  wifiManager.process();
+  
+  if (effect==1) {
+    if (millis()%10==0) { 
+      if (smer==0) {
+        analogWrite(redPin, realRed--);
+      } else {
+        analogWrite(redPin, realRed++);
+      }
+    }
+
+    if (smer==0 && realRed==0) {
+      smer=1;
+    }
+
+    if (smer==1 && realRed==255) {
+      smer=0;
+    }
+  } else if (effect==2) {
+    if (millis()%1000==0) {
+      analogWrite(redPin,   random(0,255));
+      analogWrite(bluePin,  random(0,255));
+      analogWrite(greenPin, random(0,255));
+    }
+  }
   
 } //loop
 
@@ -322,49 +184,27 @@ void sendState() {
   // client.publish(light_state_topic, buffer, true);
 }
 
-bool sendStatisticMQTT(void *) {
-  DEBUG_PRINTLN(F("Statistic"));
 
-  SenderClass sender;
-  sender.add("VersionSW", VERSION);
-  sender.add("Napeti",  ESP.getVcc());
-  sender.add("HeartBeat", heartBeat++);
-  sender.add("RSSI", WiFi.RSSI());
-  DEBUG_PRINTLN(F("Calling MQTT"));
-  
-  sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base);
-  return true;
-}
-
-void sendNetInfoMQTT() {
-  DEBUG_PRINTLN(F("Net info"));
-
-  SenderClass sender;
-  sender.add("IP",              WiFi.localIP().toString().c_str());
-  sender.add("MAC",             WiFi.macAddress());
-  
-  DEBUG_PRINTLN(F("Calling MQTT"));
-  
-  sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base);
-  return;
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    if (lastConnectAttempt == 0 || lastConnectAttempt + connectDelay < millis()) {
-      DEBUG_PRINT("Attempting MQTT connection...");
-      // Attempt to connect
-      if (client.connect(mqtt_base, mqtt_username, mqtt_key)) {
-        DEBUG_PRINTLN("connected");
-        client.subscribe((String(mqtt_base) + "/#").c_str());
-      } else {
-        lastConnectAttempt = millis();
-        DEBUG_PRINT("failed, rc=");
-        DEBUG_PRINTLN(client.state());
-      }
+bool reconnect(void *) {
+  if (!client.connected()) {
+    DEBUG_PRINT("Attempting MQTT connection...");
+    if (client.connect(mqtt_base, mqtt_username, mqtt_key, (String(mqtt_base) + "/LWT").c_str(), 2, true, "offline", true)) {
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_topic_restart)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_topic_netinfo)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_config_portal)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_config_portal_stop)).c_str());
+      client.subscribe((String(mqtt_base) + "/" + String(mqtt_topic_load)).c_str());
+      client.publish((String(mqtt_base) + "/LWT").c_str(), "online", true);
+      DEBUG_PRINTLN("connected");
+    } else {
+      DEBUG_PRINT("disconected.");
+      DEBUG_PRINT(" Wifi status:");
+      DEBUG_PRINT(WiFi.status());
+      DEBUG_PRINT(" Client status:");
+      DEBUG_PRINTLN(client.state());
     }
   }
+  return true;
 }
 
 bool processJson(String message) {
@@ -374,15 +214,33 @@ bool processJson(String message) {
 
   DynamicJsonDocument doc(1024);
   deserializeJson(doc, json);
-  realRed = doc["r"];
-  DEBUG_PRINTLN(realRed);
-  realBlue = doc["be"];
-  DEBUG_PRINTLN(realBlue);
-  realGreen = doc["g"];
-  DEBUG_PRINTLN(realGreen);
-  analogWrite(redPin, realRed);
-  analogWrite(bluePin, realBlue);
-  analogWrite(greenPin, realGreen);
+  String e = doc["effect"];
+  DEBUG_PRINTLN(e);
+  if (e=="null") {
+    //{effect:"",r:100,g:0,b:0}
+    realRed = doc["r"];
+    DEBUG_PRINTLN(realRed);
+    realBlue = doc["b"];
+    DEBUG_PRINTLN(realBlue);
+    realGreen = doc["g"];
+    DEBUG_PRINTLN(realGreen);
+    analogWrite(redPin, realRed);
+    analogWrite(bluePin, realBlue);
+    analogWrite(greenPin, realGreen);
+  } else if (e=="heartbeat") {
+    //{effect:"heartbeat"}
+    effect = 1;
+  } else if (e=="random") {
+    //{effect:"random"}
+    effect = 2;
+  } else if (e=="stop") {
+    //{effect:"stop"}
+    effect = 0;
+    analogWrite(redPin, 0);
+    analogWrite(bluePin, 0);
+    analogWrite(greenPin, 0);
+  }
+
  
 
   // if (root.containsKey("state")) {
@@ -470,4 +328,18 @@ bool processJson(String message) {
   // }
 
   return true;
+}
+
+
+void breath(int t, int p, int d) {
+  if (t==0) { //up and down
+    for (uint8_t i=0; i<255; i++) {
+      analogWrite(p, i);
+      delay(d);
+    }
+    for (uint8_t i=255; i>0; i--) {
+      analogWrite(p, i);
+      delay(d);
+    }
+  }
 }
